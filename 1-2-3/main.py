d24bb055a02ca5c40ede2c9601264c4f7f4ff570
mrkiedes <akiedes@vtsd.com>
2024-11-15 13:06:25 -0500

    1.2.3 Day 1

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -1,28 +1,41 @@
-#   a123_apple_1.py
+##############################################################################
+#   a123_TR_apple_typing_2.py
+#   Run code in VS Code to be able to access the images.
+#   Example soulution:
+#      Code connects to the image of an apple.
+#      The apple is located on the image of a tree.
+#      The apple does not draw a line as it falls.
+#      When A is pressed, the letter A appears on the apple.
+#      The apple falls to the ground when the A key is pressed.
+##############################################################################
 import turtle as trtl
 
-#-----setup-----
 apple_image = "apple.gif" # Store the file name of your shape
+ground_height = -200
+apple_letter_x_offset = -25
+apple_letter_y_offset = -50
 
 wn = trtl.Screen()
 wn.setup(width=1.0, height=1.0)
 wn.addshape(apple_image) # Make the screen aware of the new file
+
 wn.bgpic("background.gif")
 apple = trtl.Turtle()
+apple.penup()
 
-#-----functions-----
+
 # given a turtle, set that turtle to be shaped by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
   wn.update()
 
-def apple_down(active_apple):
-  x_coor = active_apple.xcor()
-  y_coor = active_apple.ycor()
-  active_apple.goto(x_coor, y_coor - 300)
+def drop_apple():
+  apple.goto(apple.xcor(), ground_height)
 
-#-----function calls-----
+
+# Function Calls
 draw_apple(apple)
-apple_down(apple)
+wn.onkeypress(drop_apple, "a")
 
-wn.mainloop()
+wn.listen()
+trtl.mainloop()
