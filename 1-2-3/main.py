f944761fb1f8210704e87d891025eadf2c255850
mrkiedes <akiedes@vtsd.com>
2024-11-18 11:55:51 -0500

    1.2.3 Day 2 Update

--- 1-2-3/main.py
+++ 1-2-3/main.py
@@ -1,5 +1,5 @@
 ##############################################################################
-#   a123_TR_apple_typing_2.py
+#   a123_TR_apple_typing_3.py
 #   Run code in VS Code to be able to access the images.
 #   Example soulution:
 #      Code connects to the image of an apple.
@@ -7,6 +7,7 @@
 #      The apple does not draw a line as it falls.
 #      When A is pressed, the letter A appears on the apple.
 #      The apple falls to the ground when the A key is pressed.
+#      The apple and letter dissapear after the apple hits the ground.
 ##############################################################################
 import turtle as trtl
 
@@ -22,18 +23,33 @@ wn.addshape(apple_image) # Make the screen aware of the new file
 wn.bgpic("background.gif")
 apple = trtl.Turtle()
 apple.penup()
+wn.tracer(False)
 
-
-# given a turtle, set that turtle to be shaped by the image file
+# given a turtle, active_apple, set that turtle to be shaped
+# by the image file
 def draw_apple(active_apple):
   active_apple.shape(apple_image)
+  draw_letter("A", active_apple)
   wn.update()
 
+# This function moves the apple to the ground and hides it.
 def drop_apple():
+  wn.tracer(True)
   apple.goto(apple.xcor(), ground_height)
+  apple.clear()
+  apple.hideturtle()
+  wn.tracer(False)
 
 
-# Function Calls
+# letter is of type str
+# active_apple is a turtle
+def draw_letter(letter, active_apple):
+  active_apple.color("white")
+  remember_position = active_apple.position()
+  active_apple.setpos(active_apple.xcor() + apple_letter_x_offset,active_apple.ycor() + apple_letter_y_offset)
+  active_apple.write(letter, font=("Arial", 74, "bold"))
+  active_apple.setpos(remember_position)
+
 draw_apple(apple)
 wn.onkeypress(drop_apple, "a")
 
